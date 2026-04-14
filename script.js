c96337238e438a033b5f7a27d6a626fd7c3b81c8
Merlin Hernandez <83794388+MerlinHdz@users.noreply.github.com>
2026-04-13 22:53:05 -0500

    create initial files

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+function operate(operator, a, b) {
+    return;
+}
+
+
+function add(a, b) {
+    return a + b;
+}
+
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+// testinggg
+console.log("Hello");
